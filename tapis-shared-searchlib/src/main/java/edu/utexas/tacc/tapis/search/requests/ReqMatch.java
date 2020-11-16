e43fc599818b4775de9f0c27b05d8deb47799833
Steve Black <scblack@tacc.utexas.edu>
2020-11-16 13:40:36 -0600

    Updates for support of Systems matchConstraints endpoint

---
+++ tapis-shared-searchlib/src/main/java/edu/utexas/tacc/tapis/search/requests/ReqMatch.java
@@ -0,0 +1,9 @@
+package edu.utexas.tacc.tapis.search.requests;
+
+/*
+ * Class representing an incoming POST match constraints request json body
+ */
+public final class ReqMatch
+{
+  public String[] match; // List of strings representing an SQL-like WHERE clause
+}
