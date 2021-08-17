7c88f1224e8f8fe4c36847c7662d192e73f7305a
jagaither <jgaither@tacc.utexas.edu>
2021-08-17 09:34:03 -0500

    Removed unused imports and functions for the path sanitizer

--- tapis-shared-lib/src/main/java/edu/utexas/tacc/tapis/shared/utils/PathSanitizer.java
+++ tapis-shared-lib/src/main/java/edu/utexas/tacc/tapis/shared/utils/PathSanitizer.java
@@ -1,11 +1,7 @@
 package edu.utexas.tacc.tapis.shared.utils;
 
-import java.io.IOException;
-import java.net.URI;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-
-import org.apache.commons.io.FilenameUtils;
 
 public class PathSanitizer{
 	//STATIC VARIABLES
@@ -14,35 +10,15 @@ public class PathSanitizer{
 	final static String slashPattern = "/";
 	final static Pattern slashPatt = Pattern.compile(slashPattern);
 	//Regex pattern that returns true if the string being checked DOES NOT
-    //contain any of the chars: $, &, >, |, ;, `
+    //contain any of the chars: &, >, <, |, ;, `
 	final static String regPattern = "[^&><|;`]+";
 	final static Pattern regPatt = Pattern.compile(regPattern);
 	final static String[] dangPatterns = {"..","&&","||","\n",};
 	
+	//Method to parse paths by regex pattern of "/". This returns each element of a path 
+	//delimited by "/" in a regular string array.
 	public static String[] usingSplit(String path) {
 		return slashPatt.split(path);
-	}
-	
-	
-	//Not very useful for path sanitizing
-	//Checks for ".." and "." and recognizes them, but it then returns
-	//the path with ".." and "." executed meaning /home/bud/../mydir/myfile
-	//returns /home/mydir/myfile
-	//
-    //FilenameUtils class also not very helpful for recognizing 
-    //dangerous characters like ";","|",">x"
-	
-	public static void usingFilenameUtils() throws IOException {
-	    String path = "/home/bud/../mydir/myfile.txt";
-	    System.out.println("Full Path: " +FilenameUtils.getFullPath(path));
-	    System.out.println("Relative Path: " +FilenameUtils.getPath(path));
-	    System.out.println("Prefix: " +FilenameUtils.getPrefix(path));
-	    System.out.println("Extension: " + FilenameUtils.getExtension(path));
-	    System.out.println("Base: " + FilenameUtils.getBaseName(path));
-	    System.out.println("Name: " + FilenameUtils.getName(path));
-
-	    String filename = "/home/bud/../mydir/./myfile";
-	    System.out.println("Normalized Path: " + FilenameUtils.normalize(filename));
 	}
 	
 	
@@ -68,7 +44,7 @@ public class PathSanitizer{
 	//Method to check user supplied strings for presence of ".." path traversal pattern.
 	//This function is intended to be used to cleanse any string that makes its 
 	//way to the command line directly and should not allow for unwanted path traversal.
-	//returns true if special case is detected and string should be rejected
+	//Returns true if special case ".." is detected and string should be rejected.
 	public static boolean splitAndCheckForParent(String pathIn) {
 		String[] contents = PathSanitizer.usingSplit(pathIn);
 	    boolean containsChar = false;
