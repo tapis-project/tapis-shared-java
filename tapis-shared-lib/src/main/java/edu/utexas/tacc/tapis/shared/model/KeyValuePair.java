dbefa459ee87ab11edb5b40838f0f5939ff3495c
Richard Cardone <rcardone@tacc.utexas.edu>
2023-06-09 16:17:42 -0500

    more envVars enhancements

--- tapis-shared-lib/src/main/java/edu/utexas/tacc/tapis/shared/model/KeyValuePair.java
+++ tapis-shared-lib/src/main/java/edu/utexas/tacc/tapis/shared/model/KeyValuePair.java
@@ -2,11 +2,13 @@ package edu.utexas.tacc.tapis.shared.model;
 
 public class KeyValuePair 
 {
-    private String key;
-    private String value;
-    private String description;
+    private String  key;
+    private String  value;
+    private String  description;
+    private Boolean include;
+    private Object  notes;
     
-    public String getKey() {
+	public String getKey() {
         return key;
     }
     public void setKey(String key) {
@@ -24,4 +26,16 @@ public class KeyValuePair
     public void setDescription(String description) {
         this.description = description;
     }
+	public Boolean getInclude() {
+		return include;
+	}
+	public void setInclude(Boolean include) {
+		this.include = include;
+	}
+    public Object getNotes() {
+		return notes;
+	}
+	public void setNotes(Object notes) {
+		this.notes = notes;
+	}
 }
