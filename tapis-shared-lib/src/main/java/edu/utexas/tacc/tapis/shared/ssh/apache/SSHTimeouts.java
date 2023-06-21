b88ce3027cc1c9a5100fd620ee375d6095b63008
Richard Cardone <rcardone@tacc.utexas.edu>
2023-06-21 13:55:07 -0500

    doubled SSHTimeouts

--- tapis-shared-lib/src/main/java/edu/utexas/tacc/tapis/shared/ssh/apache/SSHTimeouts.java
+++ tapis-shared-lib/src/main/java/edu/utexas/tacc/tapis/shared/ssh/apache/SSHTimeouts.java
@@ -4,9 +4,9 @@ public class SSHTimeouts
 {
     // Default values.
     public static final long MAX_WAIT = Long.MAX_VALUE;
-    public static final long DEFAULT_CONNECT_MILLIS = 15000;
-    public static final long DEFAULT_AUTHENTICATE_MILLIS = 10000;
-    public static final long DEFAULT_OPEN_CHANNEL_MILLIS = 10000;
+    public static final long DEFAULT_CONNECT_MILLIS = 30000;
+    public static final long DEFAULT_AUTHENTICATE_MILLIS = 20000;
+    public static final long DEFAULT_OPEN_CHANNEL_MILLIS = 20000;
     public static final long DEFAULT_EXECUTION_MILLIS = 600000; // 10 minutes.
     
     // Timed operations.
